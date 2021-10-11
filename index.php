a9be21402d7ccf8a17271694213f21005f1a72fe
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-10-11 15:25:10 +0200

    Comment cicle foreach

--- index.php
+++ index.php
@@ -69,5 +69,12 @@ Gestire il “Database” e la visualizzazione di queste domande e risposte con 
                'In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l\'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l\'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell\'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell\'URL referrer potrebbero essere disponibili mediante Google Analytics o un\'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all\'esatte parole chiave che hanno determinato il clic su un annuncio.' 
             ]
         ]
-    ]
+    ];
+
+    // foreach ($faqs as $faq) {
+    //     echo '<h2>' . $faq['question'] . '</h2>';
+    //     for ($i = 0; $i < count($faq['answer']); $i++) {
+    //         echo '<p>' . $faq['answer'][$i] . '</p>';
+    //     }
+    // }
 ?>
